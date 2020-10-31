f58f17b36dfd15fbc23a3c1acb16ef4564a2f790
RavilAxmadyllin <craftwaymiks@gmail.com>
2020-11-01 00:56:38 +0300

    add scroll to bottom

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,5 +1,5 @@
-import {Button, Card, CardContent, FormControl, Paper, TextField, Typography, Grid} from '@material-ui/core'
-import React, {useState} from 'react'
+import {Button, Card, CardContent, FormControl, Grid, Paper, TextField, Typography} from '@material-ui/core'
+import React, {useEffect, useRef, useState} from 'react'
 import {useStyles} from '../App'
 import {useDispatch, useSelector} from 'react-redux'
 import {clientTyping, sentMessage} from '../reducer/chat-reducer'
@@ -8,6 +8,7 @@ import {AppStateType} from '../bll/store'
 export const Cards = () => {
     const messages = useSelector((state: AppStateType) => state.chat.messages)
     const user = useSelector((state: AppStateType) => state.chat.userType)
+    const messagesAnchorRef = useRef<HTMLDivElement>(null)
     const [value, setValue] = useState('')
     const dispatch = useDispatch()
     const classes = useStyles()
@@ -19,22 +20,29 @@ export const Cards = () => {
     const clientTypingHandler = () => {
         dispatch(clientTyping())
     }
+
+    useEffect(() => {
+        messagesAnchorRef.current?.scrollIntoView({behavior: 'smooth'})
+    }, [messages])
+
     return (
         <Grid item xs={12}>
             <Paper className={classes.wrapper}>
 
                 <Card className={classes.content}>
                     <CardContent>
-                        {messages.map((m: any, i: number) => <Typography
+                        {messages.map((m, i) => <Typography
                             key={new Date().getTime().toString() + i}
                             className={classes.messageBlock}
                             variant={'subtitle2'}
-                            gutterBottom><b>{m.user.name} </b>: {m.message}</Typography>
+                            gutterBottom>
+                            <b>{m.user.name} </b>: {m.message}</Typography>
                         )}
+                        <div ref={messagesAnchorRef}></div>
                     </CardContent>
                 </Card>
 
-                {user.map((u: any) => <Typography
+                {user.map((u) => <Typography
                     variant={'body2'}
                     className={classes.typeUser}>{u.name} typing...</Typography>)}
 
