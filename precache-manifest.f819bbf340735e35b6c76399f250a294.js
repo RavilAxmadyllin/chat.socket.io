e1dc0d112d53abc6d91ec024cede868ffccd6ea7
RavilAxmadyllin <craftwaymiks@gmail.com>
2020-10-31 22:03:50 +0300

    Updates

---
+++ precache-manifest.f819bbf340735e35b6c76399f250a294.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cdfc8870a043e0889c6744cd003086",
+    "url": "/chat.socket.io/index.html"
+  },
+  {
+    "revision": "70ec342b52dce53d488c",
+    "url": "/chat.socket.io/static/css/main.5ededee0.chunk.css"
+  },
+  {
+    "revision": "5d4138fdfb18a171c295",
+    "url": "/chat.socket.io/static/js/2.f694c61d.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/chat.socket.io/static/js/2.f694c61d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ec342b52dce53d488c",
+    "url": "/chat.socket.io/static/js/main.f9193dbf.chunk.js"
+  },
+  {
+    "revision": "33b0ca83dc63c4a28feb",
+    "url": "/chat.socket.io/static/js/runtime-main.56159de2.js"
+  }
+]);
